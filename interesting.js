ff3ab1f3a0ca889fcd2b663adbd8418ed880b372
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2025-09-28 23:06:02 +0400

    feat: humor + simple code structure improvement

--- interesting.js
+++ interesting.js
@@ -1,14 +1,15 @@
-require("dotenv").config();
 const axios = require("axios");
+const convertXmlToJson = require("./src/helpers/convertXmlToJson");
+require("dotenv").config();
 const { wpJsonPath, wpXmlPath } = require("./src/constants/filePaths");
 
-const STRAPI_URL = `${process.env.STRAPI_URL}/api/humors`;
+const STRAPI_URL = `${process.env.STRAPI_URL}/api/interestings`;
 
 async function migrateHumors() {
     await convertXmlToJson(wpXmlPath, wpJsonPath);
     const items = transformData(wpJsonPath, "interesting");
 
-    for (const { title, humor } of items) {
+    for (const { title, interesting } of items) {
         try {
             const res = await axios.post(STRAPI_URL, {
                 headers: {
@@ -16,7 +17,7 @@ async function migrateHumors() {
                 },
                 data: {
                     title,
-                    humor,
+                    interesting,
                 },
             });
 
--- src/helpers/transformData.js
+++ src/helpers/transformData.js
@@ -1,99 +1,27 @@
-const axios = require("axios");
-const fsp = require("fs").promises;
-const fs = require("fs");
-const path = require("path");
-const FormData = require("form-data");
-const convertToMarkdown = require("./convertToMarkdown");
+const transformData = async (inputFile, key) => {
+    try {
+        const data = await fsp.readFile(inputFile, "utf-8");
+        const parsedData = JSON.parse(data);
 
-const downloadImage = async (url, filepath) => {
-  const response = await axios({
-    url,
-    responseType: "stream",
-  });
+        const items = parsedData.rss.channel[0].item;
 
-  const writer = fs.createWriteStream(filepath);
+        const res = [];
+        for (const item of items) {
+            const title = item.title[0];
+            const content = convertToMarkdown(
+                item["content:encoded"][0]
+            ).markdown;
 
-  response.data.pipe(writer);
+            res.push({
+                title: title,
+                [key]: content,
+            });
+        }
 
-  return new Promise((resolve, reject) => {
-    writer.on("finish", resolve);
-    writer.on("error", reject);
-  });
-};
+        await fsp.unlink(inputFile);
 
-const uploadImage = async (imageUrl) => {
-  try {
-    const imageName = imageUrl.split("/").pop();
-    const tempFilePath = path.resolve(__dirname, imageName);
-
-    await downloadImage(imageUrl, tempFilePath);
-
-    const form = new FormData();
-    form.append("files", fs.createReadStream(tempFilePath));
-
-    const uploadResponse = await axios.post(
-      `${process.env.STRAPI_URL}/upload`,
-      form,
-      {
-        headers: {
-          ...form.getHeaders(),
-          Authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
-        },
-      }
-    );
-
-    await fsp.unlink(tempFilePath);
-    return uploadResponse.data[0];
-  } catch (e) {
-    console.log(`Error with uploading image: ${imageUrl}`);
-  }
-};
-
-const getTransformedData = async (wpData) => {
-  let transformedData = [];
-  let attachment_url = "";
-
-  for (const item of wpData) {
-    const content = item["content:encoded"][0];
-    const { markdown, fbPost } = convertToMarkdown(content);
-
-    if (item["wp:attachment_url"]) {
-      attachment_url = item["wp:attachment_url"][0];
-      continue;
+        return res;
+    } catch (err) {
+        console.error("Error transforming JSON file:", err);
     }
-
-    const uploadedImage = await uploadImage(attachment_url);
-
-    transformedData.push({
-      title: item.title[0],
-      articleText: [{ __component: "text.paragraph", paragraph: markdown }],
-      publishDate: item["wp:post_date"][0],
-      slug: item["wp:post_name"][0],
-      fbPost: fbPost,
-      url: item["wp:post_name"][0],
-      mainImage: uploadedImage?.id || null,
-    });
-  }
-
-  return transformedData;
 };
-
-const transformData = async (inputFile, outputFile) => {
-  try {
-    const data = await fsp.readFile(inputFile, "utf-8");
-    const parsedData = JSON.parse(data);
-
-    const wpContents = parsedData.rss.channel[0].item;
-    const reversed = wpContents.reverse();
-
-    const transformedData = await getTransformedData(reversed);
-    const dataJSON = JSON.stringify(transformedData, null, 4);
-    
-    await fsp.writeFile(outputFile, dataJSON);
-    await fsp.unlink(inputFile);
-  } catch (err) {
-    console.error("Error transforming JSON file:", err);
-  }
-};
-
-module.exports = transformData;
