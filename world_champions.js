31c810c127123e904261fa2c94d3df656a887d87
Artiom Suqiasyan <artiom.suqiasyan1401@gmail.com>
2025-11-10 23:00:29 +0400

    feat: add champions and presidents

---
+++ world_champions.js
@@ -0,0 +1,15 @@
+const convertXmlToJson = require("./src/helpers/convertXmlToJson");
+const transformData = require("./src/helpers/transformData");
+const upload = require("./src/helpers/upload");
+const { wpJsonPath, wpXmlPath } = require("./src/constants/filePaths");
+
+async function migrateArmenianChampions() {
+    await convertXmlToJson(wpXmlPath, wpJsonPath);
+    const items = await transformData(wpJsonPath);
+
+    for await (const { title, content } of items) {
+        await upload("/api/world-champions", { title, content });
+    }
+}
+
+migrateArmenianChampions();
